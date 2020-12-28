0f894bafae5fc0a268868bf2a336e589797ea181
Jarom Plewe <jplewe23@gmail.com>
2020-12-28 12:28:28 -0700

    added past route to create a new workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,22 @@
 var db = reqire("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
             .then(workout => {
                 res.json(workout);
             })
             .catch(err => res.json(err));
-    });
+    })
+
+    app.post("/api/workouts", async (req, res) => {
+        try {
+            const response = await db.Workout.create({ type: 'workout' })
+            res.json(response)
+        }
+        catch (err) {
+            console.log("could not create the workout: ", err)
+        }
+    })
 
 };
